e1cbaf976de584b15185805195de0f74d3c76b17
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-03-11 20:32:44 -0400

    got the format down

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,14 +4,20 @@ function Login(){
   return(
     <div>
       <form>
-        <label>Username<label>
+        <label>Username</label>
+        <br />
         <input type="text" placeholder="Username" />
         <br />
+        <br />
         <label>Password</label>
+        <br />
         <input type="password" />
+        <br />
         <br />
         <button type="Submit">Sign In</button>
       </form>
     </div>
   )
 }
+
+export default Login
