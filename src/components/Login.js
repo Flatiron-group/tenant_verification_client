627cbf431f7b4c3cd0aa7d83edc9c0616ff70ce8
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-03-11 22:52:59 -0400

    got the attributes for the username and password down. need to work on handling the inputs

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login(){
+  const [ username, setUsername ] = useState("")
+  const [ password, usePassword ] = useState("")
+
+  const handleChange = e => {
+    console.log("meep")
+  }
+
   return(
     <div>
       <form>
         <label>Username</label>
         <br />
-        <input type="text" placeholder="Username" />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={handleChange}
+        />
         <br />
         <br />
         <label>Password</label>
         <br />
-        <input type="password" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handleChange}
+        />
         <br />
         <br />
         <button type="Submit">Sign In</button>
